43022a9890847b840e3e2a1e1e71cc447da3edb1
andytyler <atyler1@sheffield.ac.uk>
2018-03-22 16:57:05 +0000

    added in search page

--- routes/search.js
+++ routes/search.js
@@ -7,8 +7,8 @@ var jeff = setInterval(pickWord(words), 1000);
 var words = ["Search...", "Keyword", "Restraunt Name", "Cuisine i.e.Chineese", "postcode"];
 function pickWord(wordsArray) {
     var rand = wordsArray[math.floor(Math.random() * wordsArray.length)];
-    var pickedWord = wordsArray[rand]
-    return pickedWord
+    var pickedWord = wordsArray[rand];
+    return pickedWord;
 }
 
 /* GET home page. */
