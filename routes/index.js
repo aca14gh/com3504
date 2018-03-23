0fc48ba10428f52093e4aefd695c100c31e18d5f
andytyler <atyler1@sheffield.ac.uk>
2018-03-23 15:16:31 +0000

    added in search page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+
+/* GET home page. */
+
+var words = ["Search...", "Keyword", "Restraunt Name", "Cuisine i.e.Chinese", "postcode"];
+
+var jeff = pickWord(words);
+
+function pickWord(wordsArray) {
+    var rand = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    var pickedWord = wordsArray[rand];
+    return pickedWord;
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Class' });
+    res.render('Index', { jeff: jeff, title: 'My Class' });
 });
+
 
 
 /* GET home page. */
--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-var jeff = setInterval(pickWord(words), 1000);
 
-var words = ["Search...", "Keyword", "Restraunt Name", "Cuisine i.e.Chineese", "postcode"];
-function pickWord(wordsArray) {
-    var rand = wordsArray[math.floor(Math.random() * wordsArray.length)];
-    var pickedWord = wordsArray[rand];
-    return pickedWord;
-}
 
 /* GET home page. */
 router.get('/welcome', function(req, res, next) {
